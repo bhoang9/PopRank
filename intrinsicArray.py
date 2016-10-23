efe406f8fe4c156bfad0584f4df44811bb7cb28b
Brian H <brianbhoang9@gmail.com>
2016-10-23 11:30:46 -0700

    intrinsic array
    initializes intrinsic array. Size determined by interval and endtime.

---
+++ intrinsicArray.py
@@ -0,0 +1,12 @@
+import random
+
+endTime = 100
+interval = 5
+length = endTime/interval 
+intrinsicArray = [0] * length
+
+for i in range (0,index):
+    x = random.randrange(0,99)
+    intrinsicArray[i] = x
+    print (intrinsicArray[i])
+
